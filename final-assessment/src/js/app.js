ec0335cc88d295049f7cbe900e51e2dba23ca6ed
Ivan Gabrovsky <ivangabrovsky@Ivans-MacBook-Pro.local>
2021-04-14 16:31:03 -0400

    fixed formatting

--- final-assessment/src/js/app.js
+++ final-assessment/src/js/app.js
@@ -33,7 +33,7 @@ function showObservations(observations, title) {
 function showAll() {
   // Get all the observations from our data.js and format them so we can work with the data
   const observations = getAllObservations();
-  showObservations(observations, 'All Species');
+  showObservations(observations, "All Species");
 }
 
 // Show native species
@@ -43,7 +43,7 @@ function showOnlyNative() {
   // Filter out any that aren't native species
   const native = filterOnlyNative(observations);
 
-  showObservations(native, 'Only Native Species');
+  showObservations(native, "Only Native Species");
 }
 
 // Show introduced species
@@ -53,7 +53,7 @@ function showOnlyIntroduced() {
   // Filter out any that aren't introduced species
   const introduced = filterOnlyIntroduced(observations);
 
-  showObservations(introduced, 'Only Introduced Species');
+  showObservations(introduced, "Only Introduced Species");
 }
 
 function start() {
@@ -73,13 +73,13 @@ function start() {
   btnOnlyIntroduced.onclick = () => showOnlyIntroduced();
 
   // Connect the search form
-  const searchForm = document.querySelector('form');
-  searchForm.onsubmit = function(event) {
+  const searchForm = document.querySelector("form");
+  searchForm.onsubmit = function (event) {
     // Stop the form from trying to submit over the network. We'll process the data here.
     event.preventDefault();
 
     // TODO - get the value from the search form input and pass it to the
-    const searchValue = document.getElementById('search-text').value;
+    const searchValue = document.getElementById("search-text").value;
     searchValue && search(searchValue);
     // search() function (already defined in js/search.js)
     // console.log('TODO - call search() function with value from search form:', searchValue);
@@ -87,6 +87,6 @@ function start() {
 
   // Show all observations at first
   showAll();
-};
+}
 
 window.onload = start;
--- final-assessment/src/js/validate.js
+++ final-assessment/src/js/validate.js
@@ -2,65 +2,67 @@ function validate(event) {
   // TODO - write custom validation logic to validate the longitude and latitude
   // values. The latitude value must be a number between -90 and 90; the
   // longitude value must be a number between -180 and 180. If either/both are
-  // invalid, show the appropriate error message in the form, and stop the 
+  // invalid, show the appropriate error message in the form, and stop the
   // form from being submitted. If both values are valid, allow the form to be
   // submitted.
-  
+
   // Latitute validation
-  const latitude = document.getElementById('latitute').value;
+  const latitude = document.getElementById("latitute").value;
   const latitudeValid = latitude && (latitude >= -90 || latitude <= 90);
-  let latInvalidMessage = document.getElementById('latitute-invalid');
+  let latInvalidMessage = document.getElementById("latitute-invalid");
   if (!latitudeValid) {
     event.preventDefault();
-    latInvalidMessage.removeAttribute('hidden');
-  } else{
-    latInvalidMessage.setAttribute('hidden', 'true');
+    latInvalidMessage.removeAttribute("hidden");
+  } else {
+    latInvalidMessage.setAttribute("hidden", "true");
   }
-  
+
   // Longitude Validation
-  const longitude = document.getElementById('longitude').value;
+  const longitude = document.getElementById("longitude").value;
   const longitudeValid = longitude && (longitude >= -180 || longitude <= 180);
-  let lonInvalidMessage = document.getElementById('longitude-invalid');
+  let lonInvalidMessage = document.getElementById("longitude-invalid");
   if (!longitudeValid) {
     event.preventDefault();
-    lonInvalidMessage.removeAttribute('hidden');
-  } else{
-    lonInvalidMessage.setAttribute('hidden', 'true');
+    lonInvalidMessage.removeAttribute("hidden");
+  } else {
+    lonInvalidMessage.setAttribute("hidden", "true");
   }
-  
+
   // Name validation
-  const name = document.getElementById('name').value;
-  let nameInvalidMessage = document.getElementById('name-invalid');
+  const name = document.getElementById("name").value;
+  let nameInvalidMessage = document.getElementById("name-invalid");
   if (!name) {
     event.preventDefault();
-    nameInvalidMessage.removeAttribute('hidden');
+    nameInvalidMessage.removeAttribute("hidden");
   } else {
-    nameInvalidMessage.setAttribute('hidden', 'true');
+    nameInvalidMessage.setAttribute("hidden", "true");
   }
 
   // Description validation
-  const description = document.getElementById('description').value;
-  let descriptionInvalidMessage = document.getElementById('description-invalid');
+  const description = document.getElementById("description").value;
+  let descriptionInvalidMessage = document.getElementById(
+    "description-invalid"
+  );
   if (!description) {
     event.preventDefault();
-    descriptionInvalidMessage.removeAttribute('hidden');
+    descriptionInvalidMessage.removeAttribute("hidden");
   } else {
-    descriptionInvalidMessage.setAttribute('hidden', 'true');
+    descriptionInvalidMessage.setAttribute("hidden", "true");
   }
 
   // date_observed validation
-  const date_observed = document.getElementById('date_observed').value;
-  let dateObservedInvalidMessage = document.getElementById('date-invalid');
+  const date_observed = document.getElementById("date_observed").value;
+  let dateObservedInvalidMessage = document.getElementById("date-invalid");
   if (!date_observed) {
     event.preventDefault();
-    dateObservedInvalidMessage.removeAttribute('hidden');
+    dateObservedInvalidMessage.removeAttribute("hidden");
   } else {
-    dateObservedInvalidMessage.setAttribute('hidden', 'true');
+    dateObservedInvalidMessage.setAttribute("hidden", "true");
   }
-  
-  if(latitudeValid && longitudeValid && name && description && date_observed){
+
+  if (latitudeValid && longitudeValid && name && description && date_observed) {
     return true;
-  } else{
+  } else {
     event.preventDefault();
     console.log("invalid form");
     //alert('Please fill all required fields with valid data');
@@ -69,7 +71,7 @@ function validate(event) {
 }
 
 // Wait for the window to load, then set up the submit event handler for the form.
-window.onload = function() {
-  const form = document.querySelector('form');
+window.onload = function () {
+  const form = document.querySelector("form");
   form.onsubmit = validate;
 };
