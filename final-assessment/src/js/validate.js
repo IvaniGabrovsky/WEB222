26e37b74e9d00efdc39fd3e60af870f19fc6188f
Ivaylo Gabrovsky <ivaylo.gabrovsky@telus.com>
2021-04-13 22:21:13 -0400

    add form validation

--- final-assessment/src/js/validate.js
+++ final-assessment/src/js/validate.js
@@ -5,13 +5,26 @@ function validate(event) {
   // invalid, show the appropriate error message in the form, and stop the 
   // form from being submitted. If both values are valid, allow the form to be
   // submitted.
-  if(latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180){
-    event.onload
+  const latitude = document.getElementById('latitute').value;
+  const longitude = document.getElementById('longitude').value;
+  const latitudeValid = latitude && (latitude >= -90 || latitude <= 90);
+  const longitudeValid = longitude && (longitude >= -180 || longitude <= 180);
+  if(!latitudeValid || !longitudeValid){
+    event.preventDefault();
+    if (!latitudeValid) {
+      // show error for latitude
+      console.log('Invalid latitude');
+    }
+    if (!longitudeValid) {
+      // show error for longitude
+      console.log('Invalid longitude');
+    }
+    return false;
+  } else {
+    console.log('Form is valid. Submit to formspree.io');
+    console.log('latitude=', latitude);
+    return true;
   }
-  else{
-    xhr.open("GET", "http://example.com");
-  }
-  console.log('TODO - validate the longitude, latitude values before submitting');
 }
 
 // Wait for the window to load, then set up the submit event handler for the form.
